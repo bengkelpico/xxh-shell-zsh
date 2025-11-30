fb98c7bf6654a56367ec871c04d7ef31bc764fd6
Julian Prasetyo <jul14n4v@gmail.com>
2025-11-30 19:19:15 +0700

    Update Latest ZSH

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ done
 cp $CDIR/zshrc $build_dir/.zshrc
 
 # tag=$(curl --silent https://api.github.com/repos/romkatv/zsh-bin/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
-tag=v3.0.1
+tag=v6.1.1
 arch=$(uname -m)
 if [[ $arch == x86_64* ]]; then
 	distfile=zsh-5.8-linux-x86_64
@@ -53,12 +53,4 @@ else
 fi
 
 tar -xzf $tarname
-if [[ $arch == x86_64* ]]; then
-mv zsh-5.8-linux-x86_64/* .
-elif  [[ $arch == arm* ]]; then
-mv zsh-5.8-linux-armv7l/* .
-elif  [[ $arch == aarch* ]]; then
-mv zsh-5.8-linux-aarch64/* .
-fi
-
 rm $tarname
